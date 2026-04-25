8c1552323383473103bf14613b81c3b9c313b31f
Divine Demon <shadowloard04@gmail.com>
2026-04-25 14:28:20 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from ytSearch import VideosSearch
 from AnonXMusic.utils.formatters import time_to_seconds
 import aiohttp
 from AnonXMusic import LOGGER
