e47c729ea67813cad8b8e44a4fb8030f19bde8e2
Divine Demon <shadowloard04@gmail.com>
2026-04-25 14:06:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/cookies2002/Samtest",
+    "https://github.com/DivineDemonn/RaspberryRhythm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
